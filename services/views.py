1d2fe57caf50efa6c5b6e64288e512b2b4007750
r-fine <saiadul.arfain@tuta.io>
2021-12-03 02:39:17 +0600

    HomeView,  CategoryListView, DetailView

--- services/views.py
+++ services/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render, get_object_or_404, HttpResponse
 from django.contrib.auth.decorators import login_required, user_passes_test
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from allauth.account.decorators import verified_email_required
 
 from .models import *
 
 
-# @verified_email_required
-def home(request):
-    categories = Category.objects.all()
-    subcategories = Category.objects.filter(level=1)
+class HomeView(ListView):
+    model = Category
+    template_name = 'service/home.html'
+    context_object_name = 'subcategories'
+    paginate_by = 4
+    queryset = Category.objects.filter(level=1)
 
-    return render(request, 'home/home.html', {'categories': categories, 'subcategories': subcategories})
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "service/category-list.html"
 
 
-def category_details(request):
-    pass
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = "service/category-detail.html"
+    context_object_name = 'category'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'services': Service.objects.filter(category=self.object)
+        })
+        return context
