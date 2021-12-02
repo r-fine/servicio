185f806639e39659bbb7f44442ec40ba552e3695
r-fine <saiadul.arfain@tuta.io>
2021-12-03 02:37:38 +0600

    order form

--- orders/models.py
+++ orders/models.py
@@ -11,6 +11,58 @@ class Order(models.Model):
         ('Completed', 'Completed'),
         ('Cancelled', 'Cancelled'),
     )
+    AREA = (
+        ('Abdullahpur', 'Abdullahpur'),
+        ('Agargaon', 'Agargaon'),
+        ('Badda', 'Badda'),
+        ('Banani', 'Banani'),
+        ('Banasree', 'Banasree'),
+        ('Baridhara', 'Baridhara'),
+        ('Bashundhara', 'Bashundhara'),
+        ('Bawnia', 'Bawnia'),
+        ('Beraid', 'Beraid'),
+        ('Cantonment area', 'Cantonment area'),
+        ('Dakshinkhan', 'Dakshinkhan'),
+        ('Dania', 'Dania'),
+        ('Demra', 'Demra'),
+        ('Dhanmondi', 'Dhanmondi'),
+        ('Farmgate', 'Farmgate'),
+        ('Gabtali', 'Gabtali'),
+        ('Gulshan', 'Gulshan'),
+        ('Hazaribagh', 'Hazaribagh'),
+        ('Islampur', 'Islampur'),
+        ('Jurain', 'Jurain'),
+        ('Kafrul', 'Kafrul'),
+        ('Kamalapur', 'Kamalapur'),
+        ('Kamrangirchar', 'Kamrangirchar'),
+        ('Kazipara', 'Kazipara'),
+        ('Khilgaon', 'Khilgaon'),
+        ('Khilkhet', 'Khilkhet'),
+        ('Kotwali', 'Kotwali'),
+        ('Lalbagh', 'Lalbagh'),
+        ('Matuail', 'Matuail'),
+        ('Mirpur', 'Mirpur'),
+        ('Mohakhali', 'Mohakhali'),
+        ('Mohammadpur', 'Mohammadpur'),
+        ('Motijheel', 'Motijheel'),
+        ('Nimtoli', 'Nimtoli'),
+        ('Pallabi', 'Pallabi'),
+        ('Paltan', 'Paltan'),
+        ('Ramna', 'Ramna'),
+        ('Rampura', 'Rampura'),
+        ('Sabujbagh', 'Sabujbagh'),
+        ('Sadarghat', 'Sadarghat'),
+        ('Satarkul', 'Satarkul'),
+        ('Shahbagh', 'Shahbagh'),
+        ('Sher-e-Bangla Nagar', 'Sher-e-Bangla Nagar'),
+        ('Shyampur', 'Shyampur'),
+        ('Sutrapur', 'Sutrapur'),
+        ('Tejgaon', 'Tejgaon'),
+        ('Uttara', 'Uttara'),
+        ('Uttarkhan', 'Uttarkhan'),
+        ('Vatara', 'Vatara'),
+        ('Wari', 'Wari'),
+    )
 
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name='order', on_delete=models.SET_NULL, null=True
@@ -18,12 +70,22 @@ class Order(models.Model):
     order_number = models.CharField(max_length=20)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=11)
-    email = models.EmailField(max_length=100)
+    phone = models.CharField(verbose_name='Phone Number', max_length=11)
+    email = models.EmailField(verbose_name='Email Address', max_length=100)
     address_line_1 = models.CharField(max_length=255)
     address_line_2 = models.CharField(max_length=255, blank=True)
-    area = models.CharField(max_length=50, blank=True)
+    area = models.CharField(max_length=50, choices=AREA)
     order_note = models.CharField(max_length=100, blank=True)
+    date = models.DateField(
+        verbose_name='Delivery Date',
+        auto_now=False,
+        auto_now_add=False,
+    )
+    time = models.TimeField(
+        verbose_name='Delivery Time',
+        auto_now=False,
+        auto_now_add=False,
+    )
     status = models.CharField(max_length=10, choices=STATUS, default='New')
     ordered = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
 
-# Create your views here.
+from allauth.account.decorators import verified_email_required
+
+from .models import Order
+from .forms import OrderForm
+
+import datetime
+
+
+class OrderCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    template_name = "order/order-form.html"
+    form_class = OrderForm
+    success_url = reverse_lazy('services:home')
+    success_message = 'Order placed successfully'
+
+    def form_valid(self, form):
+        year = int(datetime.date.today().strftime('%Y'))
+        month = int(datetime.date.today().strftime('%m'))
+        date = int(datetime.date.today().strftime('%d'))
+        s = int(datetime.datetime.now().strftime('%S'))
+        if len(str(s)) == 1:
+            s = '6'+str(s)
+        else:
+            s = str(s)
+        order_date = datetime.date(year, month, date)
+        current_date = order_date.strftime("%Y%m%d")
+        order_number = current_date + str(form.instance.phone[7:]) + s
+
+        form.instance.user = self.request.user
+        form.instance.order_number = order_number
+
+        return super().form_valid(form)
